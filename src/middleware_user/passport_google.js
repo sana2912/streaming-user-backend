85c2beb9697ce0c0b8eff3ae967b77765b7f1d7d
sana2912 <path25230@gmail.com>
2025-08-21 03:23:40 +0700

    config google oauth2.0 redirect url

--- src/middleware_user/passport_google.js
+++ src/middleware_user/passport_google.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const google_strategy = new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/api/user/google/auth"
+    callbackURL: process.env.GOOGLE_REDIRECT_URL,
 },
     async function (accessToken, refreshToken, profile, cb) {
         return cb(null, profile);
